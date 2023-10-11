2278dfacf84ffc08a2068b32d679776d5b9f0e00
emines0 <emin.sefa1@gmail.com>
2023-10-11 16:26:28 +0200

    add feedback

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import FeedbackData from './Data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import { v4 as uuidv4 } from 'uuid'
 
 function App() {
    const [feedback, setFeedback] = useState(FeedbackData)
@@ -13,11 +14,17 @@ function App() {
          setFeedback(feedback.filter((item) => item.id !== id))
       }
    }
+
+   const addFeedback = (newFeedback) => {
+      newFeedback.id = uuidv4()
+      setFeedback([newFeedback, ...feedback])
+   }
+
    return (
       <>
          <Header />
          <div className='container'>
-            <FeedbackForm />
+            <FeedbackForm handleAdd={addFeedback} />
             <FeedbackStats feedback={feedback} />
             <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
          </div>
